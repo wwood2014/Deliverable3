c448f02c15b8399565f2d919eefc43d0f787501b
wwood2014 <wesleywood2412@fullsail.edu>
2014-09-18 19:24:25 -0400

    Added jason data.

---
+++ Wood_Wesley_Project3/json.js
@@ -0,0 +1,41 @@
+/*
+JSON
+Wesley Wood
+SDI 201409
+Project 3
+*/
+
+var jsonstr = {
+    "People": [
+        {
+            "Person1": [
+                {
+                    "Name": "Kevin"
+                },
+                {
+                    "Age" = 37
+                }
+            ]
+        },
+        {
+            "Person2": [
+                {
+                    "Name": "Ryan"
+                },
+                {
+                    "Age": 28
+                }
+            ]
+        },
+        {
+            "Person3": [
+                {
+                    "Name": "Wesley"
+                },
+                {
+                    "Age": 24
+                }
+            ]
+        }
+    ]
+};
