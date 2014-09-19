ec790c6fd9c1a6a04aaed8bdb4a9c8579e17e503
wwood2014 <wesleywood2412@fullsail.edu>
2014-09-18 21:06:07 -0400

    Added arguments aand functions.

--- Wood_Wesley_Project3/Wood_Wesley_Project3.js
+++ Wood_Wesley_Project3/Wood_Wesley_Project3.js
@@ -13,10 +13,11 @@ var myName = "Wesley";
 var miles = 10;
 var place = true;
 var place2 = false;
+var returnValue;
 
 function inputFriends()
 {
-    console.log("Hey " + friend1 + ", hurry up and get here.");
+    console.log("\n\nHey " + friend1 + ", hurry up and get here.");
     var poles = prompt("How many fishing poles do you have?");
     console.log("You have brought " + poles + " to go fishing with.");
     
@@ -50,3 +51,41 @@ function milesDriven()
 };
 
 console.log("My friends" + friend1 + " and " + friend2 + "came fishing with me today.");
+console.log("\We decided to go fishing at the Bokeelia Pier.");
+console.log(friend1 + " decided to use cut bait for fishing.");
+console.log(friend2 + " decided to use shrimp as bait.");
+console.log(friend1 + friend2 + myName + " were ready to fish.");
+
+var read = prompt("Are you ready to continue?");
+{
+        if (read == "Yes" || read == "yes")
+        {
+            console.log("\n\nCatch the biggest fish you can!");
+        }
+        else if (read == "No" || read == "no")
+        {
+            console.log("\n\nTake as much time as you need.");
+        }
+        else
+        {
+         console.log("\n\nAre you ready?");
+        }
+}
+
+if (place === true)
+{
+        timeDriven = timeDriven + 15;
+        if (place2 === true)
+        {
+                timeDriven = timeDriven + 15;
+                console.log("Since we're going fishing at both places, we will be driving" + timeDriven + " minutes total.");
+        }
+        else
+        {
+                console.log("Since we're only going fishing at Bokeelia, we'll only be driving " + timeDriven + "minutes.");    
+        }
+}
+else
+{
+        console.log("Your total time driven stayed the same at " + timeDriven + " minutes.");
+}
